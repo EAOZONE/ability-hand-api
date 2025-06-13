ebead0351c8743fe49dcd100384908fadca90af4
Ben Pratt <c.ben.pratt@gmail.com>
2025-06-13 12:40:24 -0500

    Splits write at least

--- cpp/hand_wave.cpp
+++ cpp/hand_wave.cpp
@@ -5,25 +5,30 @@
 
 #include "wrapper.h"
 
-int main(int argc, char *argv[]) {
-  AHWrapper wrapper = AHWrapper(0x50, 921600);
-  wrapper.connect();
+int main(int argc, char* argv[]) {
+    AHWrapper wrapper = AHWrapper(0x50, 1000000);
+    wrapper.connect();
 
-  auto now = std::chrono::steady_clock::now();
-  auto duration = std::chrono::duration<double>(now.time_since_epoch());
-  std::array<float, 6> cmd = {30.0, 30.0, 30.0, 30.0, 30.0, -30};
+    auto now = std::chrono::steady_clock::now();
+    auto duration = std::chrono::duration<double>(now.time_since_epoch());
+    std::array<float, 6> cmd = { 30.0, 30.0, 30.0, 30.0, 30.0, -30 };
 
-  for (size_t i = 0; i < 100000; i++) {
+    for (size_t i = 0; i < 10000; i++) {
 
-    // Calculate Hand Wave
-    now = std::chrono::steady_clock::now();
-    duration = std::chrono::duration<double>(now.time_since_epoch());
-    for (size_t j = 0; j < cmd.size(); ++j) {
-      double ft = static_cast<double>(duration.count()) * 3.0 +
-                  j * (2.0 * 3.14159265359 / 12.0);
-      cmd[j] = (0.5 * std::sin(ft) + 0.5) * 45.0 + 15.0;
+        // Calculate Hand Wave
+        now = std::chrono::steady_clock::now();
+        duration = std::chrono::duration<double>(now.time_since_epoch());
+        for (size_t j = 0; j < cmd.size(); ++j) {
+            double ft = static_cast<double>(duration.count()) * 3.0 +
+                j * (2.0 * 3.14159265359 / 12.0);
+            cmd[j] = (0.5 * std::sin(ft) + 0.5) * 45.0 + 15.0;
+        }
+        cmd[5] = -cmd[5];
+        auto t0 = std::chrono::high_resolution_clock::now();
+        wrapper.write_once(cmd, POSITION, 0);
+        auto t1 = std::chrono::high_resolution_clock::now();
+        auto us = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count();
+        std::printf("write time = %llu \n", (unsigned long long)us);
+
     }
-    cmd[5] = -cmd[5];
-    wrapper.read_write_once(cmd, POSITION, 0);
-  }
 }
